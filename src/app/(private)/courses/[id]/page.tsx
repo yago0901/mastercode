ebf74f2f47d6a6a1805122c978427cdfa8fb793e
Yago Santos Gigeck <yago0901@gmail.com>
2025-06-29 19:15:52 -0300

    fix video course

--- src/app/(private)/courses/[id]/page.tsx
+++ src/app/(private)/courses/[id]/page.tsx
@@ -40,7 +40,7 @@ export default function Page() {
   }
 
   return (
-    <div className="p-6 mx-auto w-full h-[100%] flex flex-col bg-[rgba(95,0,191,0.1)]">
+    <div className="p-6 mx-auto w-full h-[100%] flex flex-col-reverse bg-[rgba(95,0,191,0.1)]">
       <h1 className="text-2xl font-bold mb-4 w-full text-[var(--text)]">Vídeos do Curso {courseId}</h1>
 
       <div className="flex flex-col lg:flex-row gap-4 w-full flex-grow ">
@@ -71,11 +71,10 @@ export default function Page() {
         </div>
       </div>
 
-      <div className="flex justify-end mt-4">
+      <div className="flex justify-end">
         <button
           onClick={() => setIsAddModalOpen((prev) => !prev)}
           className="bg-[var(--primary)] text-[var(--text-white)] px-4 py-2 rounded transition shadow-lg cursor-pointer"
-          style={{ pointerEvents: 'all' }} 
         >
           Adicionar Vídeo
         </button>
